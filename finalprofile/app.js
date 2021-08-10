ec96027f34b27f4499d0ff04823b783d3d9963c5
sajidamullani <85106150+sajidamullani@users.noreply.github.com>
2021-08-10 10:55:28 +0530

    Add files via upload

---
+++ finalprofile/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "20px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "#f1f1f1";
+}
